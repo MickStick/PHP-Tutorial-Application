5b39cd7d8777d93b1ba186a83493e78781755ff7
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-08-01 11:38:25 -0500

    just a quick commit

--- profile.php
+++ profile.php
@@ -18,6 +18,15 @@ $title = $_SESSION["fname"]. " " .$_SESSION["lname"];
         <main id="profile-view">
             <img src="<?php echo $_SESSION["propic"];?>" alt="face">
             <h1>Shaw's username is <?php echo $_SESSION["username"];?></h1>
+            <p>
+                <?php
+                    $propicAppend = "";
+                    for($x = 0; $x < 3; $x++){ 
+                        parse_str( $propicAppend .= $_SESSION["id"][strlen($_SESSION["id"]) - ($x+1)]); 
+                    }
+                    echo $propicAppend;
+                ?>
+            </p>
         </main>
     </body>
 </html>
