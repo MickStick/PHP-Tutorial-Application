bb52e712740443995992d2d05dc1b7c6365451bf
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-08-12 10:32:07 -0500

    change db connection to heroku hosted db

--- model/DBConfig.php
+++ model/DBConfig.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'test_php');
+define('DB_SERVER', 'us-cdbr-iron-east-05.cleardb.net');//define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'bd787d6c551941');//define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '43ab084c');//define('DB_PASSWORD', '');
+define('DB_DATABASE', 'heroku_1df9baf112de306');//define('DB_DATABASE', 'test_php');
 $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 if (!$conn){
